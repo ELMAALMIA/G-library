6a7f032e07fe726cb82444fe510bba20f877ba38
ELMAALMIA <a.elmaalmi@edu.umi.ac.ma>
2024-10-22 22:44:31 +0100

    feat: update package.json and other files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,62 @@
-
- function main(){
-    let db = new MySqlRepository();
-    db.open()
-    .then(msg=>console.log(msg))
-    .catch(err=> console.log("  Rerr", err));
+import MySqlRepository from "./repositories/mysql-repository.js";
 
 
-    db.close
+ 
+let data = {
+    "Author":"EL MAALMI",
+    "Year_Born": 2001
+    }
+
+
+
+function testConexion(){
+  
+}
+
+
+function testSelect(){
+    db.select('authors').then(
+        (data)=>console.log(data),
+
+    ).catch(
+        (err)=>console.log(err)
+    )
+}
+
+
+function testInsert(data){
+  
+
+        if( data.Au_ID==null){
+            data.Au_ID=currentId
+        }
+        db.insert('authors',data);
+
+}
+
+function testUpdate(data, id, idColumn){
+    db.update(
+        'authors',
+        data,
+        id,
+        idColumn
+    )
+
+}
+
+function testRemove(id, idColumn){
+    db.remove('authors', id, idColumn);
+}
+function findById(id, idColumn){
+    db.selectByID('authors', id, idColumn).then(
+        a=>console.log(`author id = ${id} `, a)
+    ).catch(
+    err=> console.log("Error ",err)
+);
+}
+
+function main(){
+    testSelect();
 }
 
 main();
